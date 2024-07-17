8899684120f943323d35790b081c40c93fc58b22
Yuri Zamboni <yurizp@hotmail.com>
2024-07-17 16:33:08 -0300

    first commit

---
+++ src/main/java/br/com/yurizp/utilsmvc/UtilsMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.yurizp.utilsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/yurizp/utilsmvc/UtilsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yurizp.utilsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
